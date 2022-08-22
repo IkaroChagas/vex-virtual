7f6cf280478df6d888e038a9a7b4f9d635f56d92
Ikaro Chagas <ikarochagas.ig@gmail.com>
2022-08-22 13:54:22 -0300

    JS added at form

--- dashboard/script-dashboard.js
+++ dashboard/script-dashboard.js
@@ -1,30 +1,58 @@
-const form = document.getElementById('subs')
-const ageData = document.getElementsByClassName('inputAge');
-const codeData = document.getElementsByClassName('inputCode');
-const usernameData = document.getElementsByClassName('inputName');
-const cityData = document.getElementsByClassName('inputCity');
-const emailData = document.getElementsByClassName('inputEmail')
+const form = document.querySelector('form')
 
-
-form.addEventListener('submit', (e) => {
-   e.preventDefault()
-
-    checkInput()
+form.addEventListener('submit', e => {
+    e.preventDefault()
+     const userName = document.getElementById('name').value.trim()
+     const userAge = document.getElementById('age').value.trim()
+     const userCity = document.getElementById('city').value.trim()
+     const userCode = document.getElementById('code').value.trim()
+     const userEmail = document.getElementById('email').value.trim()
     
+    const errors = []
+
+    if(userName === '') 
+        errors.push('Usuário não pode ficar em branco')
+    
+    if(userAge === '')
+        errors.push('Idade não pode ficar em branco')
+    
+    if(userCity === '')
+        errors.push('O campo cidade não pode ficar em branco')
+    
+    if(userCode === '')
+        errors.push('Insira o seu código da rede de voo')
+    
+    if(userEmail === '')
+        errors.push('Campo e-mail não pode ficar em branco')
+
+    if(errors.length > 0) {
+
+        for(let i = 0; i < errors.length; i++){
+                Toastify ({
+                        text: errors[i],
+                        duration: 4000,
+                        gravity: "top",
+                        position: 'center',
+                        style: {
+                         background: "#DF1C24"
+                        }
+                }).showToast()
+        }
+    } else {
+        Toastify({
+                text: "Enviado com sucesso!",
+                duration: 4000,
+                gravity: 'top',
+                position: 'center',
+                style: {
+                        background: '#4bab4e'
+                }
+        }).showToast()
+    }
 })
 
-function checkInput() {
-    let userName = usernameData.value.trim()
-    let age = ageData.value.trim()
-    let city = cityData.value.trim()
-    let code = codeData.value.trim()
-    let email = emailData.value.trim()
-
-    if(userName, age, city, code, email === ''){
-            setError('Campo não pode está vazio')
-    } else {
-            setSuccess('Campo preenchido com sucesso!')
-    }
+function backtoMenu() {
+        window.location = '/default.aspx';
 }
 
-setSuccess(input)
+       
